065651aa71dc16d554df452d014d25de74c48c3b
Star-Man <stuartmcdonald91@yahoo.co.uk>
2018-10-27 12:54:58 +0100

    Initial commit

---
+++ Source/UE4_BattleTank/UE4_BattleTankGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4_BattleTankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_BATTLETANK_API AUE4_BattleTankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
